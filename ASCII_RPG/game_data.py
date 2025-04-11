126211920eaea161d43e398ffb979dedb542c299
Terry C <terrycarlisle77@yahoo.com>
2025-04-11 12:10:55 -0400

    Add files via upload

---
+++ ASCII_RPG/game_data.py
@@ -0,0 +1,15 @@
+############################################################
+#This is for global data that gets passed between files
+#
+#
+#
+#
+##############################################################
+
+# base stats
+pot = 1 #potions
+ATK = 1 # extra atk power
+Luck_Chance = 5 # evade percent
+megapotion = 0
+
+
